7777e67ba575ebe304425e7322f08c6646a18928
KimHyeonsang <asakqjqtk123@gmail.com>
2021-09-01 15:14:19 +0900

    API

---
+++ APIFramework/API_Framework/Bullet.cpp
@@ -0,0 +1,49 @@
+#include "Bullet.h"
+#include"Player.h"
+
+Bullet::Bullet() : Count(0)
+{
+
+}
+
+Bullet::~Bullet()
+{
+
+}
+
+
+
+void Bullet::Initialize()
+{
+	TransInfo.Position = Vector3(0.0f, 0.0f);
+	TransInfo.Scale = Vector3(30.0f, 30.0f);
+
+	strKey = "Bullet";
+	Count = 0;
+	Active = false;
+
+	Speed = 5.0f;
+}
+
+int Bullet::Update()
+{
+	TransInfo.Position.x += Speed;
+
+	if (TransInfo.Position.x > 1280)
+		return 1;
+	return 0;
+}
+
+void Bullet::Render(HDC _hdc)
+{
+	Ellipse(_hdc,
+		int(TransInfo.Position.x - (TransInfo.Scale.x / 2)),
+		int(TransInfo.Position.y - (TransInfo.Scale.x / 2)),
+		int(TransInfo.Position.x + (TransInfo.Scale.x / 2)),
+		int(TransInfo.Position.y + (TransInfo.Scale.x / 2)));
+}
+
+void Bullet::Release()
+{
+
+}
